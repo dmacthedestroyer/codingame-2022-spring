f78502dc5c34cbf68efa508d2606e464f1534dff
dmacthedestroyer <mcdonald.daniel.james@gmail.com>
2022-04-25 00:04:20 -0700

    berserker

--- src/main.ts
+++ src/main.ts
@@ -1,144 +1,193 @@
 import {
-  Coordinate,
-  countBy,
-  distance,
-  distanceSquared,
-  Hero,
-  minBy,
-  Monster,
-  sortBy,
-  State,
-  translate,
+	Base,
+	Coordinate,
+	distance,
+	distanceSquared,
+	Hero,
+	minBy,
+	Monster,
+	sortBy,
+	State
 } from "./state";
 
-function action3(state: State): string[] {
-  const aggressiveMonsters = state.monsters.filter(
-    (monster) => monster.isTargetingBase === "self"
-  );
-  const closestToBase = minBy(aggressiveMonsters, (m) =>
-    distanceSquared(m.position, state.basePosition)
-  );
-  return state.selfHeroes.map((hero, i) => {
-    if (i === 0) {
-      return goalie(hero, state);
-    }
-
-    const closestMonster = minBy(aggressiveMonsters, (m) =>
-      distanceSquared(m.position, hero.position)
-    );
-    if (closestMonster !== undefined) {
-      return `MOVE ${closestMonster.position.x +
-        closestMonster.vx} ${closestMonster.position.y +
-        closestMonster.vy} >:-O`;
-    } else {
-      const delta = 2000 * (state.basePosition.x === 0 ? 1 : -1);
-      const dx = delta * (state.selfHeroes.length - i);
-      const dy = delta * (i + 1);
-
-      return `MOVE ${state.basePosition.x + dx} ${state.basePosition.y +
-        dy} ಠ_ಠ`;
-    }
-  });
+function silver(state: State): string[] {
+	return state.selfHeroes.map((hero, i) => {
+		if (i === 0) {
+			return goalie(hero, state);
+		}
+		if (i === 1) {
+			return berserker(hero, state);
+		}
+		return defaultGuy(hero, state);
+	});
 }
 
 function goalie(hero: Hero, state: State): string {
-  const targetPosition = state.basePosition;
+	if (state.selfBase.mana < 10) {
+		return defaultGuy(hero, state);
+	}
 
-  const windDistance = 1280;
-  const closestMonster = minBy(state.monsters, (m) =>
-    distanceSquared(targetPosition, m.position)
-  );
-  if (closestMonster !== undefined) {
-    const monsterFuturePosition = {
-      x: closestMonster.position.x + closestMonster.vx,
-      y: closestMonster.position.y + closestMonster.vy,
-    };
-    if (distance(monsterFuturePosition, targetPosition) < windDistance / 4) {
-      return `SPELL WIND ${closestMonster.position.x} ${closestMonster.position.y} FUS`;
-    }
-    return `MOVE ${monsterFuturePosition.x} ${monsterFuturePosition.y} GRRR`;
-  }
+	const basePosition = state.selfBase.position;
 
-  return `MOVE ${targetPosition.x} ${targetPosition.y} ಠ_ಠ`;
+	const closestMonster = minBy(state.monsters, m =>
+		distanceSquared(basePosition, m.position)
+	);
+	if (closestMonster !== undefined) {
+		const monsterFuturePosition = {
+			x: closestMonster.position.x + closestMonster.vx,
+			y: closestMonster.position.y + closestMonster.vy
+		};
+		const monsterDistanceFromBase = distance(
+			monsterFuturePosition,
+			basePosition
+		);
+		if (
+			state.selfBase.mana >= 10 &&
+			monsterDistanceFromBase < WIND_DISTANCE * 0.75
+		) {
+			return `SPELL WIND ${closestMonster.position.x} ${closestMonster.position.y} FUS`;
+		}
+	}
+
+	return `MOVE ${basePosition.x} ${basePosition.y} ಠ_ಠ`;
 }
 
+function berserker(hero: Hero, state: State): string {
+	const xMin = MAP_WIDTH / 4;
+	const xMax = (3 * MAP_WIDTH) / 4;
+	function isWithinSlice(position: Coordinate): boolean {
+		return position.x >= xMin && position.x <= xMax;
+	}
+	if (!isWithinSlice(hero.position)) {
+		return `MOVE ${MAP_WIDTH / 2} ${MAP_HEIGHT / 2} OOB`;
+	}
+
+	if (state.selfBase.mana < 50) {
+		const closestMonster = minBy(
+			state.monsters.filter(m => isWithinSlice(m.position)),
+			m => distanceSquared(m.position, hero.position)
+		);
+		if (closestMonster !== undefined) {
+			return `MOVE ${closestMonster.position.x} ${closestMonster.position.y} FFUUU`;
+		}
+
+		return `MOVE ${MAP_WIDTH / 2} ${MAP_HEIGHT / 2}`;
+	}
+
+	const targetMonster = minBy(
+		state.monsters.filter(
+			m => distance(m.position, hero.position) <= CONTROL_RANGE
+		),
+		m => -m.health
+	);
+	if (
+		targetMonster !== undefined &&
+		distance(targetMonster.position, hero.position) <= CONTROL_RANGE
+	) {
+		return `SPELL CONTROL ${targetMonster.id} ${state.enemyBase.position.x} ${state.enemyBase.position.y} ZAP`;
+	}
+
+	return `WAIT zZz`;
+}
+
+function defaultGuy(
+	hero: Hero,
+	state: State,
+	defaultPosition?: Coordinate
+): string {
+	const aggressiveMonsters = state.monsters.filter(
+		monster => monster.isTargetingBase === "self"
+	);
+	const closestMonster = minBy(aggressiveMonsters, m =>
+		distanceSquared(m.position, hero.position)
+	);
+	if (closestMonster !== undefined) {
+		return `MOVE ${closestMonster.position.x +
+			closestMonster.vx} ${closestMonster.position.y +
+			closestMonster.vy} \>:-O`;
+	} else if (defaultPosition === undefined) {
+		const delta = 2000 * (state.selfBase.position.x === 0 ? 1 : -1);
+		const dx = delta;
+		const dy = delta;
+
+		return `MOVE ${state.selfBase.position.x + dx} ${state.selfBase.position.y +
+			dy} ಠ_ಠ`;
+	} else {
+		return `MOVE ${defaultPosition.x} ${defaultPosition.y} ಠ_ಠ`;
+	}
+}
+
+const WIND_DISTANCE = 1280;
+const CONTROL_RANGE = 2200;
+const MAP_WIDTH = 17630;
+const MAP_HEIGHT = 9000;
+
 export function run() {
-  /**
-   * Auto-generated code below aims at helping you parse
-   * the standard input according to the problem statement.
-   **/
+	/**
+	 * Auto-generated code below aims at helping you parse
+	 * the standard input according to the problem statement.
+	 **/
 
-  var inputs: string[] = readline().split(" ");
-  const baseX: number = parseInt(inputs[0]); // The corner of the map representing your base
-  const baseY: number = parseInt(inputs[1]);
-  const heroesPerPlayer: number = parseInt(readline()); // Always 3
+	var inputs: string[] = readline().split(" ");
+	const baseX: number = parseInt(inputs[0]); // The corner of the map representing your base
+	const baseY: number = parseInt(inputs[1]);
+	const heroesPerPlayer: number = parseInt(readline()); // Always 3
 
-  const basePosition: Coordinate = { x: baseX, y: baseY };
+	const basePosition: Coordinate = { x: baseX, y: baseY };
 
-  // game loop
-  while (true) {
-    const selfHeroes: Hero[] = [];
-    const monsters: Monster[] = [];
+	// game loop
+	while (true) {
+		const selfHeroes: Hero[] = [];
+		const monsters: Monster[] = [];
 
-    for (let i = 0; i < 2; i++) {
-      var inputs: string[] = readline().split(" ");
-      const health: number = parseInt(inputs[0]); // Your base health
-      const mana: number = parseInt(inputs[1]); // Ignore in the first league; Spend ten mana to cast a spell
-    }
-    const entityCount: number = parseInt(readline()); // Amount of heros and monsters you can see
-    for (let i = 0; i < entityCount; i++) {
-      var inputs: string[] = readline().split(" ");
-      const id: number = parseInt(inputs[0]); // Unique identifier
-      const type: number = parseInt(inputs[1]); // 0=monster, 1=your hero, 2=opponent hero
-      const x: number = parseInt(inputs[2]); // Position of this entity
-      const y: number = parseInt(inputs[3]);
-      const shieldLife: number = parseInt(inputs[4]); // Ignore for this league; Count down until shield spell fades
-      const isControlled: number = parseInt(inputs[5]); // Ignore for this league; Equals 1 when this entity is under a control spell
-      const health: number = parseInt(inputs[6]); // Remaining health of this monster
-      const vx: number = parseInt(inputs[7]); // Trajectory of this monster
-      const vy: number = parseInt(inputs[8]);
-      const nearBase: number = parseInt(inputs[9]); // 0=monster with no target yet, 1=monster targeting a base
-      const threatFor: number = parseInt(inputs[10]); // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+		function buildBaseState(): Omit<Base, "position"> {
+			const inputs: string[] = readline().split(" ");
+			const health: number = parseInt(inputs[0]); // Your base health
+			const mana: number = parseInt(inputs[1]); // Spend ten mana to cast a spell
+			return { health, mana };
+		}
+		const selfBase: Base = { ...buildBaseState(), position: basePosition };
+		const enemyBase: Base = {
+			...buildBaseState(),
+			position: baseX === 0 ? { x: MAP_WIDTH, y: MAP_HEIGHT } : { x: 0, y: 0 }
+		};
+		const entityCount: number = parseInt(readline()); // Amount of heros and monsters you can see
+		for (let i = 0; i < entityCount; i++) {
+			var inputs: string[] = readline().split(" ");
+			const id: number = parseInt(inputs[0]); // Unique identifier
+			const type: number = parseInt(inputs[1]); // 0=monster, 1=your hero, 2=opponent hero
+			const x: number = parseInt(inputs[2]); // Position of this entity
+			const y: number = parseInt(inputs[3]);
+			const shieldLife: number = parseInt(inputs[4]); // Count down until shield spell fades
+			const isControlled: number = parseInt(inputs[5]); // Equals 1 when this entity is under a control spell
+			const health: number = parseInt(inputs[6]); // Remaining health of this monster
+			const vx: number = parseInt(inputs[7]); // Trajectory of this monster
+			const vy: number = parseInt(inputs[8]);
+			const nearBase: number = parseInt(inputs[9]); // 0=monster with no target yet, 1=monster targeting a base
+			const threatFor: number = parseInt(inputs[10]); // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
 
-      if (type === 0) {
-        const isTargetingBase = nearBase
-          ? threatFor === 1
-            ? "self"
-            : threatFor === 2
-            ? "opponent"
-            : false
-          : false;
-        monsters.push({
-          id,
-          position: { x, y },
-          health,
-          vx,
-          vy,
-          isTargetingBase,
-        });
-      } else if (type === 1) {
-        selfHeroes.push({ id, position: { x, y } });
-      }
-    }
+			if (type === 0) {
+				const isTargetingBase = nearBase
+					? threatFor === 1
+						? "self"
+						: threatFor === 2
+						? "opponent"
+						: false
+					: false;
+				monsters.push({
+					id,
+					position: { x, y },
+					health,
+					vx,
+					vy,
+					isTargetingBase
+				});
+			} else if (type === 1) {
+				selfHeroes.push({ id, position: { x, y } });
+			}
+		}
 
-    const state: State = {
-      basePosition,
-      selfHeroes,
-      monsters,
-    };
-
-    const targetingMonsters = state.monsters
-      .filter((m) => m.isTargetingBase)
-      .map(({ id, health, position }) => ({
-        id,
-        health,
-        position,
-        distance: distance(position, state.basePosition),
-      }));
-    sortBy(targetingMonsters, (tm) => tm.distance).forEach((m) =>
-      console.error({ ...m, wind: m.distance < 1280 })
-    );
-
-    action3(state).forEach((cmd) => console.log(cmd));
-  }
+		const state: State = { selfBase, enemyBase, selfHeroes, monsters };
+		silver(state).forEach(cmd => console.log(cmd));
+	}
 }
--- src/state.ts
+++ src/state.ts
@@ -1,83 +1,90 @@
 export type Coordinate = {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 };
 
 export type Hero = {
-  id: number;
-  position: Coordinate;
+	id: number;
+	position: Coordinate;
 };
 
 export type Monster = {
-  id: number;
-  position: Coordinate;
-  health: number;
-  vx: number;
-  vy: number;
-  isTargetingBase: "self" | "opponent" | false;
+	id: number;
+	position: Coordinate;
+	health: number;
+	vx: number;
+	vy: number;
+	isTargetingBase: "self" | "opponent" | false;
+};
+
+export type Base = {
+	position: Coordinate;
+	health: number;
+	mana: number;
 };
 
 export type State = {
-  basePosition: Coordinate;
-  selfHeroes: Hero[];
-  monsters: Monster[];
+	selfBase: Base;
+	enemyBase: Base;
+	selfHeroes: Hero[];
+	monsters: Monster[];
 };
 
 export function distanceSquared(c1: Coordinate, c2: Coordinate): number {
-  const dx = c2.x - c1.x;
-  const dy = c2.y - c1.y;
+	const dx = c2.x - c1.x;
+	const dy = c2.y - c1.y;
 
-  return dx * dx + dy * dy;
+	return dx * dx + dy * dy;
 }
 
 export function distance(c1: Coordinate, c2: Coordinate): number {
-  return Math.sqrt(distanceSquared(c1, c2));
+	return Math.sqrt(distanceSquared(c1, c2));
 }
 
 export function translate(coord: Coordinate, delta: Coordinate): Coordinate {
-  return {
-    x: coord.x + delta.x,
-    y: coord.y + delta.y,
-  };
+	return {
+		x: coord.x + delta.x,
+		y: coord.y + delta.y
+	};
 }
 
 export function minBy<T>(ts: T[], f: (t: T) => number): T | undefined {
-  const initialValue: [T | undefined, number] = [undefined, Number.MAX_VALUE];
+	const initialValue: [T | undefined, number] = [undefined, Number.MAX_VALUE];
 
-  let minValue = Number.MAX_VALUE;
-  let minT: T | undefined = undefined;
+	let minValue = Number.MAX_VALUE;
+	let minT: T | undefined = undefined;
 
-  ts.forEach((t) => {
-    const value = f(t);
-    if (value < minValue) {
-      minValue = value;
-      minT = t;
-    }
-  });
+	ts.forEach(t => {
+		const value = f(t);
+		if (value < minValue) {
+			minValue = value;
+			minT = t;
+		}
+	});
 
-  return minT;
+	return minT;
 }
 
 export function sortBy<T, TValue>(ts: T[], f: (t: T) => TValue): T[] {
-  const copy = [...ts];
-  copy.sort((a, b) => {
-    const va = f(a);
-    const vb = f(b);
+	const copy = [...ts];
+	copy.sort((a, b) => {
+		const va = f(a);
+		const vb = f(b);
 
-    return va < vb ? -1 : va > vb ? 1 : 0;
-  });
+		return va < vb ? -1 : va > vb ? 1 : 0;
+	});
 
-  return copy;
+	return copy;
 }
 
 export function countBy<T, TKey>(
-  ts: T[],
-  keyFunc: (t: T) => TKey
+	ts: T[],
+	keyFunc: (t: T) => TKey
 ): Map<TKey, number> {
-  return ts.reduce((counts, t) => {
-    const key = keyFunc(t);
-    counts.set(key, (counts.get(key) ?? 0) + 1);
+	return ts.reduce((counts, t) => {
+		const key = keyFunc(t);
+		counts.set(key, (counts.get(key) ?? 0) + 1);
 
-    return counts;
-  }, new Map<TKey, number>());
+		return counts;
+	}, new Map<TKey, number>());
 }
